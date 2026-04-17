b5acfbcbb218f70369997f33a647953874862d84
grimavatar <57611284+grimavatar@users.noreply.github.com>
2026-04-17 00:22:04 -0400

    Create nemo_logging.py

---
+++ forced_aligner/utils/nemo_logging.py
@@ -0,0 +1,8 @@
+def suppress_logging():
+    # Suppress standard Python logging warnings
+    import logging
+    logging.getLogger().setLevel(logging.ERROR)
+
+    # Suppress NeMo's verbose Info (I) and Warning (W) logs
+    import nemo.utils as nemo_utils
+    nemo_utils.logging.setLevel(nemo_utils.logging.ERROR)
